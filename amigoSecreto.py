8bad959d1c121ff2c8fb1938492746f05342b28f
nataliacatundafr <ncatundafr@gmail.com>
2022-11-30 19:31:13 -0300

    first commit

--- amigoSecreto.py
+++ amigoSecreto.py
@@ -8,6 +8,8 @@ pessoas=int(input("digite a quantidade de amigos: "))#recebe uma quantidade x de
 for nome in range(pessoas):#repetição de nomes de acordo com a quantidade de pessoas digitada
     nome = input("Nome do Amigo(a) secreto: ")#nome dos amigos secretos
     lista.append(nome)# adicionando os amigos na lista
-print(random.choice(lista))# print o amigo sorteado
+
+
+print("o amigo sorteado foi:" + (random.choice(lista)))# print o amigo sorteado
 
 
